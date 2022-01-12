2a4ab36892bfae4e8538bce2ca01a9af8873da67
paulbradfo <90589413+paulbradfo@users.noreply.github.com>
2022-01-11 22:06:02 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,13 @@
+﻿////document.getElementById("btnSend").addEventListener("click", function () {
+////    alert("From" + document.getElementById("from").value +
+////        ", Subject: " + document.getElementById("subject").value +
+////        ", Message: " + document.getElementById("message").value);
+////})
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#from").val() +
+        ", Subject: " + $("#subject").val() +
+        ", Message: " + $("#message").val());
+
+    $("#picGanderson").fadeOut("slow");
+})
